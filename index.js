1f53b98a601721a452eb9bbaf3e4c57ece47360d
jihoon060410 <jihoon060410@gmail.com>
2025-07-26 23:26:11 +0900

    index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const core = require("@actions/core");
 const webhook_1 = require("@slack/webhook");
 const axios_1 = require("axios");
 const qs = require("querystring");
+
 (async () => {
     var _a;
     // Validate parameters
-    const [productId, scheduleId, seatId, webhookUrl] = [
+    const [productId, scheduleId, webhookUrl] = [
         "product-id",
         "schedule-id",
-        "seat-id",
         "slack-incoming-webhook-url",
     ].map((name) => {
         const value = core.getInput(name);
@@ -19,29 +19,40 @@ const qs = require("querystring");
         }
         return value;
     });
+
+    // 좌석 ID 배열: Floor M=1, Y=2, AE=3, S=4, PA=5
+    const seatIds = ['1', '2', '3', '4', '5'];
+
     const message = (_a = core.getInput("message")) !== null && _a !== void 0 ? _a : "티켓사세요";
     const webhook = new webhook_1.IncomingWebhook(webhookUrl);
-    const res = await axios_1.default({
-        method: "POST",
-        url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
-        params: {
-            v: "1",
-        },
-        data: qs.stringify({
-            prodId: productId,
-            scheduleNo: scheduleId,
-            seatId,
-            volume: 1,
-            selectedGradeVolume: 1,
-        }),
-    });
-    // tslint:disable-next-line
-    console.log("Got response: ", res.data);
-    if (res.data.chkResult) {
-        const link = `http://ticket.melon.com/performance/index.htm?${qs.stringify({
-            prodId: productId,
-        })}`;
-        await webhook.send(`${message} ${link}`);
+
+    for (const seatId of seatIds) {
+        try {
+            const res = await axios_1.default({
+                method: "POST",
+                url: "https://ticket.melon.com/tktapi/product/seatStateInfo.json",
+                params: {
+                    v: "1",
+                },
+                data: qs.stringify({
+                    prodId: productId,
+                    scheduleNo: scheduleId,
+                    seatId,
+                    volume: 1,
+                    selectedGradeVolume: 1,
+                }),
+            });
+            console.log(`Seat ID ${seatId} response:`, res.data);
+
+            if (res.data.chkResult) {
+                const link = `http://ticket.melon.com/performance/index.htm?${qs.stringify({
+                    prodId: productId,
+                })}`;
+                await webhook.send(`${message} 좌석 ID ${seatId} 예매 가능! ${link}`);
+            }
+        } catch (error) {
+            console.error(`Error checking seat ID ${seatId}:`, error);
+        }
     }
 })().catch((e) => {
     console.error(e.stack); // tslint:disable-line
